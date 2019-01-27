1ef0b4a54f868864127af080ab7d4fd7b2b362ac
electricSemyon <electricSemyon@gmail.com>
2019-01-28 00:26:31 +0800

    specified client`s node modules in .gitignore

---
+++ client/webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    output: {
+        path: `${ __dirname }/public/`
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        }
+      ]
+    }
+  };
