c576586c5a4a1c603c4731247dc865d17d6c6199
electricSemyon <electricSemyon@gmail.com>
2019-01-28 00:30:21 +0800

    set up react environment

--- client/src/index.js
+++ client/src/index.js
@@ -1,1 +1,9 @@
-console.log(1)
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { directive } from 'babel-types';
+
+const App = () => (
+    <div>Hello!</div>
+)
+
+ReactDOM.render(<App/>, document.querySelector('#root'))
