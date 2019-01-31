2ed03f6e9a465984615e0864cd81f521c3356ec7
electricSemyon <electricSemyon@gmail.com>
2019-02-01 00:46:26 +0800

    added eslintignores

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,27 +1,27 @@
 const path = require('path')
-const LiveReloadPlugin = require('webpack-livereload-plugin');
+const LiveReloadPlugin = require('webpack-livereload-plugin')
 
 module.exports = {
-    output: {
-        path: `${ __dirname }/public/`
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader"
-          }
+  output: {
+    path: `${ __dirname }/public/`
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
         }
-      ]
-    },
-    resolve: {
-      alias: {
-        utils: path.resolve(__dirname, '../utils')
       }
-    },
-    plugins: [
-      new LiveReloadPlugin()
     ]
-  };
+  },
+  resolve: {
+    alias: {
+      utils: path.resolve(__dirname, '../utils')
+    }
+  },
+  plugins: [
+    new LiveReloadPlugin()
+  ]
+}
