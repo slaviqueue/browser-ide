3e6684dfe895fe182ee69dd97ff6a1b0333c2a22
Slava <electricsemyon@gmail.com>
2019-03-23 19:04:33 +0200

    removed extra indentation in AppContainer style

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -10,8 +10,8 @@ import LanguageSelectionPage from './pages/LanguageSelectionPage'
 import Sandbox from './pages/SandboxPage'
 
 const AppContainer = styled.div`
-    width: 100%;
-    height: 100%;
+  width: 100%;
+  height: 100%;
 `
 
 const BreadCrump = styled.span`
